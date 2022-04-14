ff2ad9c4e12441dfa5f140e158d433c2137aea75
ICMI <xue_zhiqiang@foxmail.com>
2022-04-14 16:21:13 +0800

    feat: 对接render/增加依赖

--- packages/engine/src/adapter/index.ts
+++ packages/engine/src/adapter/index.ts
@@ -1,9 +1,3 @@
-// 这里会透出更多render
-import { Renderer as CanvasRender } from '@antv/g-mobile';
-import { Renderer as WebglRender } from '@antv/g-webgl';
-import { Renderer as SvgRender } from '@antv/g-svg';
 export { default as CanvasAdapter } from './canvas';
 export * from './interfaces';
 export * from './types';
-
-export { CanvasRender, WebglRender, SvgRender };
--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -1,5 +1,3 @@
-import { Renderer as CanvasRender } from '@antv/g-mobile';
-
 export * from './jsx';
 // export createElement 别名
 export { jsx as createElement, Fragment } from './jsx';
@@ -8,5 +6,11 @@ export { default as Component } from './component';
 export { default as Children } from './children';
 export { default as createRef } from './createRef';
 export { default as Timeline } from './timeline';
-export { CanvasRender };
+
+// 这里会透出更多render
+import { Renderer as CanvasRender } from '@antv/g-mobile-canvas';
+import { Renderer as WebglRender } from '@antv/g-mobile-webgl';
+import { Renderer as SvgRender } from '@antv/g-mobile-svg';
+export { CanvasRender, WebglRender, SvgRender };
+
 export * from './adapter';
