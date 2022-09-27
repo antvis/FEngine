d7c4e9a24b9d9bb0659aa27495f5a5ba26db2d21
tangying1027 <33517362+tangying1027@users.noreply.github.com>
2022-09-27 13:51:58 +0800

    fix: fix clip & 修改版本号 (#63)

--- packages/engine/src/types.ts
+++ packages/engine/src/types.ts
@@ -218,5 +218,35 @@ export type ElementType =
     fontVariant?: 'normal' | 'small-caps';
   }
 
+// animation interface
+export type EasingFunction = (t: number) => number;
+export type InterpolateFunction = (t: number) => any;
+
+export interface Animation {
+  // 缓动函数
+  easing?: string | EasingFunction;
+  duration: number;
+  delay?: number;
+  property?: string[];
+  // 裁剪区动画
+  isClip?: boolean;
+  // start 的 attrs
+  start?: any;
+  // end 的 attrs
+  end?: any;
+  // 每一帧的处理函数
+  onFrame?: any;
+  onEnd?: any;
+}
+
+/**
+ * 动画生命周期
+ */
+export interface AnimationCycle {
+  appear?: Animation;
+  update?: Animation;
+  destroy?: Animation;
+}
+
 export { ArcStyleProps, MarkerStyleProps, SectorStyleProps }
 
