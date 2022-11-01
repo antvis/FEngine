439f007967a3582b145c71aa19b78cf54472c319
zengyue <yezengyue@gmail.com>
2022-11-01 16:39:46 +0800

    feat: lottie 渲染到 container

--- packages/lottie/src/index.tsx
+++ packages/lottie/src/index.tsx
@@ -3,10 +3,13 @@ import { loadAnimation } from '@antv/g-lottie-player';
 
 class Lottie extends Component {
   render() {
-    const { props, context } = this;
+    const { container, props } = this;
     const { data, options } = props;
     const animation = loadAnimation(data, options);
-    animation.render(context.canvas);
+    // 因为添加到文档流是后执行的，所以这里加一个 setTimeout
+    setTimeout(() => {
+      animation.render(container);
+    }, 0);
     return null;
   }
 }
