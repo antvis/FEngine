a7dc79db9e10edb3f71288ae8c893532de29ea42
zengyue ye <yezengyue@gmail.com>
2022-11-30 11:43:01 +0800

    fix: 修复 timeline 的 props 定义 (#104)

--- packages/f-engine/src/timeline.ts
+++ packages/f-engine/src/timeline.ts
@@ -4,18 +4,22 @@ import Children from './children';
 
 export interface TimelineProps {
   /**
-   * 开始的组件索引
+   * @title 起始索引
+   * @description 开始的组件索引
    */
   start?: number;
   /**
-   * 组件播放的延迟时间
+   * @title 延迟(ms)
+   * @description 组件播放的延迟时间
    */
   delay?: number;
   /**
-   * 是否自动循环
+   * @title 自动循环
+   * @description 是否自动循环
    */
   loop?: boolean;
   /**
+   * @ignore
    * 子组件
    */
   children?: any;
