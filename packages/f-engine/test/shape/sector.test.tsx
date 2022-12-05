e82aead58c09104b2b97b35fdfbdc2f5c23bdcc1
xuying.xu <xuying.xu@alibaba-inc.com>
2022-12-05 16:58:01 +0800

    fix: sector radius

--- packages/f-engine/test/shape/sector.test.tsx
+++ packages/f-engine/test/shape/sector.test.tsx
@@ -7,14 +7,35 @@ class View extends Component {
     return (
       <sector
         style={{
-          cx: 100,
+          cx: 150,
           cy: 60,
           startAngle: 0,
           endAngle: 90,
           r: 60,
           r0: 10,
           fill: 'red',
+          stroke: 'black',
           radius: [0, 8, 8, 0],
+        }}
+      />
+    );
+  }
+}
+
+class View1 extends Component {
+  render() {
+    return (
+      <sector
+        style={{
+          cx: 60,
+          cy: 60,
+          startAngle: 0,
+          endAngle: 90,
+          r: 60,
+          r0: 10,
+          fill: 'red',
+          stroke: 'black',
+          radius: [5, 5, 30, 30],
         }}
       />
     );
@@ -26,6 +47,18 @@ describe('Sector', () => {
     const { props } = (
       <Canvas context={context}>
         <View />
+      </Canvas>
+    );
+
+    const canvas = new Canvas(props);
+    canvas.render();
+    await delay(500);
+    expect(context).toMatchImageSnapshot();
+  });
+  it('内角合并', async () => {
+    const { props } = (
+      <Canvas context={context}>
+        <View1 />
       </Canvas>
     );
 
