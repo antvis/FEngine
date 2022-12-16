b7ff56a9a66704088b4040e58978e4a516408048
tangying1027 <33517362+tangying1027@users.noreply.github.com>
2022-12-16 17:36:47 +0800

    fix: lottie数据更新 (#131)
    Co-authored-by: xuying.xu <xuying.xu@alibaba-inc.com>

--- packages/f-lottie/test/index.test.tsx
+++ packages/f-lottie/test/index.test.tsx
@@ -7,13 +7,13 @@ describe('Lottie', () => {
   it('加载 lottie 文件', async () => {
     const data1 = await (
       await fetch(
-        'https://gw.alipayobjects.com/os/OasisHub/3ccdf4d8-78e6-48c9-b06e-9e518057d144/data.json'
+        'https://gw.alipayobjects.com/os/OasisHub/3ccdf4d8-78e6-48c9-b06e-9e518057d144/data.json',
       )
     ).json();
 
     const context = createContext('', {
       width: '600px',
-      height: '600px',
+      height: '300px',
     });
 
     const { props } = (
@@ -34,5 +34,38 @@ describe('Lottie', () => {
 
     const canvas = new Canvas(props);
     await canvas.render();
+  });
+  it('lottie 数据update', async () => {
+    const data1 = await (
+      await fetch(
+        'https://gw.alipayobjects.com/os/OasisHub/3ccdf4d8-78e6-48c9-b06e-9e518057d144/data.json',
+      )
+    ).json();
+
+    const context = createContext('', {
+      width: '600px',
+      height: '300px',
+    });
+
+    const { props } = (
+      <Canvas context={context}>
+        <Lottie data={null} options={{ loop: true, autoplay: true }} />
+      </Canvas>
+    );
+
+    const canvas = new Canvas(props);
+    await canvas.render();
+
+    const { props: nextProps } = (
+      <Canvas context={context}>
+        <Lottie
+          data={data1}
+          options={{ loop: true, autoplay: true }}
+          style={{ width: 100, height: 100 }}
+        />
+      </Canvas>
+    );
+
+    await canvas.update(nextProps);
   });
 });
