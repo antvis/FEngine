6f3a4df5a67f418ea18a9011829074f5ee2811cf
zengyue <yezengyue@gmail.com>
2023-08-30 10:13:08 +0800

    chore: 调试小程序 tap

--- packages/f-my/examples/test/pages/index/index.js
+++ packages/f-my/examples/test/pages/index/index.js
@@ -1,37 +1,20 @@
-import Rect from './rect';
-import { jsx as _jsx } from "@antv/f-engine/jsx-runtime";
-Page({
-  data: {
-    data: {}
-  },
-  onReady() {
-    this.setData({
-      data: {
-        index: 1,
-        width: 100
-      }
-    });
-    // 模拟数据更新
-    setTimeout(() => {
-      this.setData({
-        data: {
-          index: 2,
-          width: 200
-        }
-      });
-    }, 2000);
-  },
-  onRenderChart(props) {
-    const {
-      data
-    } = props;
-    return _jsx(Rect, {
-      ...data
-    });
 
-    // 如果不使用 jsx, 用下面代码效果也是一样的
-    // return createElement(Chart, {
-    //   data: data,
-    // });
+Page({
+  // 一定要在 canvas onReady 之后调用
+  onCanvasReady() {
+    // 使用类似 web 的 querySelector 语法选中对应的 canvas 元素
+    // my-canvas 对应 Canvas 标签的 id
+    my.createSelectorQuery()
+      .select('#my-canvas')
+      .node()
+      .exec((res) => {
+        const canvas = res[0].node;
+        // 拿到 Canvas 之后可以使用和 web 相同的 API 来获取并调用 context
+        const ctx = canvas.getContext('2d');
+        ctx.fillRect(0, 0, 100, 100);
+      });
+  },
+  onTap(e){
+    console.log(e)
   }
 });
