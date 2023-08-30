8a3ba76902d6497f5c26689d32ee9d61629a3bc1
zengyue <yezengyue@gmail.com>
2023-08-30 16:30:19 +0800

    chore: 完善小程序 ts 工程

---
+++ packages/f-my/examples/typescript/src/index/rect.tsx
@@ -0,0 +1,55 @@
+import { Component } from '@antv/f-engine';
+
+class Rect extends Component {
+  render() {
+    const { index, width = 0 } = this.props;
+    return (
+      <group style={{ display: 'flex' }}>
+        <text
+          style={{
+            text: `${index}`,
+            fill: '#000',
+            fontSize: '30px',
+          }}
+        />
+        <rect
+          style={{
+            fill: 'red',
+            width,
+            height: '40px',
+          }}
+          animation={{
+            appear: {
+              easing: 'linear',
+              duration: 300,
+              property: ['width'],
+              start: {
+                width: 0,
+              },
+              end: {},
+            },
+            update: {
+              easing: 'linear',
+              duration: 300,
+              property: ['width'],
+            },
+          }}
+          onClick={(e) => {
+            console.log('click', e);
+          }}
+          onTouchStart={(e) => {
+            console.log('onTouchStart', e);
+          }}
+          onTouchMove={(e) => {
+            console.log('onTouchMove', e);
+          }}
+          onTouchEnd={(e) => {
+            console.log('onTouchEnd', e);
+          }}
+        />
+      </group>
+    );
+  }
+}
+
+export default Rect;
