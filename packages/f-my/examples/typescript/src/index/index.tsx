864edc130161198566ca5290b4fbaf73846a2ce9
zengyue ye <yezengyue@gmail.com>
2023-08-30 16:31:53 +0800

    chore: 完善小程序 ts 工程 (#220)

--- packages/f-my/examples/typescript/src/index/index.tsx
+++ packages/f-my/examples/typescript/src/index/index.tsx
@@ -1,16 +1,8 @@
-import Chart from './chart';
-
-const data = [
-  { genre: 'Sports', sold: 275 },
-  { genre: 'Strategy', sold: 115 },
-  { genre: 'Action', sold: 120 },
-  { genre: 'Shooter', sold: 350 },
-  { genre: 'Other', sold: 150 },
-];
+import Rect from './rect';
 
 Page({
   data: {},
   onRenderChart() {
-    return <Chart data={data} />;
+    return <Rect index={1} width={100} />;
   },
 });
