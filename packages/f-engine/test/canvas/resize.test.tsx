1c33297c889c35143555818beb144c60df2fede6
xuying <33517362+tangying1027@users.noreply.github.com>
2024-05-06 09:47:12 +0800

    fix: resize时component 挂在最新context (#266)
    Co-authored-by: xuying.xu <xuying.xu@alibaba-inc.com>

--- packages/f-engine/test/canvas/resize.test.tsx
+++ packages/f-engine/test/canvas/resize.test.tsx
@@ -2,6 +2,22 @@ import { jsx, Canvas, Component, JSX } from '../../src';
 import { createContext, delay } from '../util';
 const context = createContext();
 import { Renderer } from '@antv/g-mobile-canvas';
+const Background = (props, context) => {
+  const { layout, color } = props;
+  return (
+    <group>
+      <rect
+        style={{
+          x: 0,
+          y: 0,
+          height: context.height + context.top,
+          width: context.width + context.left,
+          fill: 'yellow',
+        }}
+      />
+    </group>
+  );
+};
 
 describe('Canvas', () => {
   it('resize', async () => {
@@ -41,6 +57,27 @@ describe('Canvas', () => {
 
     await canvas.resize(100, 50);
     await delay(200);
+
+    expect(context).toMatchImageSnapshot();
+  });
+
+  it('resize context', async () => {
+    const renderer = new Renderer();
+
+    const { props } = (
+      <Canvas renderer={renderer} context={context} width={100} height={50}>
+        <Background />
+      </Canvas>
+    );
+
+    const canvas = new Canvas(props);
+    await canvas.render();
+
+    await delay(200);
+
+    await canvas.resize(200, 100);
+    await delay(200);
+
     expect(context).toMatchImageSnapshot();
   });
 });
