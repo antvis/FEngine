4f18b05abb71723bb46036b4df00a296d17cdc88
zengyue <yezengyue@gmail.com>
2024-09-10 10:43:18 +0800

    fix: 小程序主题设置不生效

--- packages/f-my/src/index.tsx
+++ packages/f-my/src/index.tsx
@@ -62,8 +62,10 @@ Component({
   didUpdate() {
     const { canvas, props } = this;
     if (!canvas) return;
+    const { theme } = props;
     const children = props.onRender(props);
     canvas.update({
+      theme,
       children,
     });
   },
@@ -134,11 +136,13 @@ Component({
       if (!width || !height) {
         return;
       }
+      const { theme } = this.props;
       const children = this.props.onRender(this.props);
       const canvas = new Canvas({
         pixelRatio,
         width,
         height,
+        theme,
         context,
         children,
         createImage,
