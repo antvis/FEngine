a2a90e6a8a24808ee79ded640a24e1e6c13310c2
zengyue ye <yezengyue@gmail.com>
2025-08-05 11:45:56 +0800

    test(render): 添加 svg 和 webgl 事件的单测 (#240)

--- packages/f-engine/test/canvas/webgl/webgl.test.tsx
+++ packages/f-engine/test/canvas/webgl/webgl.test.tsx
@@ -1,5 +1,6 @@
 import { jsx, Canvas } from '../../../src';
 import { Renderer } from '@antv/g-mobile-webgl';
+import { delay, gestureSimulator } from '../../util';
 
 const canvasEl = document.createElement('canvas');
 canvasEl.style.display = 'block';
@@ -12,6 +13,8 @@ const context = canvasEl.getContext('webgl');
 describe('webgl', () => {
   it('webgl renderer', async () => {
     const renderer = new Renderer();
+    const onPan = jest.fn();
+    const onClick = jest.fn();
     const { props } = (
       <Canvas renderer={renderer} context={context}>
         <rect
@@ -20,11 +23,27 @@ describe('webgl', () => {
             height: '200px',
             fill: 'red',
           }}
+          onPan={onPan}
+          onClick={onClick}
         />
       </Canvas>
     );
 
     const canvas = new Canvas(props);
     await canvas.render();
+
+    await delay(500);
+    gestureSimulator(canvasEl, 'touchstart', { x: 60, y: 60 });
+    gestureSimulator(canvasEl, 'touchmove', { x: 62, y: 80 });
+    gestureSimulator(canvasEl, 'touchend', { x: 62, y: 80 });
+    await delay(500);
+
+    expect(onPan.mock.calls.length).toBe(1);
+    // move不触发click事件
+    expect(onClick.mock.calls.length).toBe(0);
+
+    await delay(100);
+    gestureSimulator(canvasEl, 'click', { x: 60, y: 60 });
+    expect(onClick.mock.calls.length).toBe(1);
   });
 });
